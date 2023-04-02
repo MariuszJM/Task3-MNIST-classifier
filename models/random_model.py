4b545fa155880b80fbcfa2d2a441418a1a2987ff
MariuszMJ <mariusz.michna.j@gmail.com>
2023-04-02 15:41:42 +0200

    Random model has been created

---
+++ models/random_model.py
@@ -0,0 +1,10 @@
+import numpy as np
+from models.digit_classification_interface import DigitClassificationInterface
+
+
+class RandomModel(DigitClassificationInterface):
+    def predict(self, input_data):
+        input_data = input_data[9:19, 9:19]
+
+        random_prediction = np.random.randint(0, 10)
+        return random_prediction
